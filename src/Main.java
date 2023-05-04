c95c81b0f3440cf195097c16b7e13c54fa0ceadc
Dmitriy <div4ik@live.ru>
2023-05-04 19:03:56 +0500

    Выброшено исключение - деление на ноль

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
 
-        System.out.println("Hello world!");
+        try {
+            System.out.println(num1/num2);
+
+        } catch (Exception e) {
+            System.out.println("Деление на ноль");
+
+        }
     }
 }
---
+++ src/WrongNumberException.java
@@ -0,0 +1,2 @@
+public class WrongNumberException extends Exception {
+}
